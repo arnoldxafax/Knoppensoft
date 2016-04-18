e3876096261f435264c9753c2392ad19d6e605fb
Arnold Hageman <ahageman@xafax.nl>
2016-04-18 16:58:04 +0200

    Knop 2 toegevoegd

--- uMain.pas
+++ uMain.pas
@@ -10,7 +10,9 @@ type
   TForm1 = class(TForm)
     Button1: TButton;
     Label1: TLabel;
+    Button2: TButton;
     procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -27,6 +29,11 @@ implementation
 procedure TForm1.Button1Click(Sender: TObject);
 begin
   Label1.Text := 'Er is op knop 1 gedrukt';
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  Label1.Text := 'Er is op knop 2 gedrukt';
 end;
 
 end.
